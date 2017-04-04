6f2066f59f07154952b167baa3fb096acfe76fda
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-04-04 10:25:58 +0200

    PR feedback

--- src/app/kiosk/kiosk.module.ts
+++ src/app/kiosk/kiosk.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { KioskRoutingModule } from './kiosk-routing.module';
+
 import { KioskComponent } from './kiosk.component';
 import { ProductsGridComponent } from './product-grid/products-grid.component';
 import { ProductCardComponent } from './product-grid/product-card/product-card.component';
