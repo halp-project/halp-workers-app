3d2e5703cd15d16bf92e6026ca75e52d4781ac26
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-04-04 13:22:00 +0200

    fix: Remove some warnings

--- src/app/patients/patients-grid/patient-card/patient-card.component.ts
+++ src/app/patients/patients-grid/patient-card/patient-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
-import { Patient } from '../Patient';
+import { Patient } from '../patient';
 
 @Component({
   selector: 'app-patient-card',
