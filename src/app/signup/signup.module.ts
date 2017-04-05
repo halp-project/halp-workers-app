a2378ce95160f8342a02c10edde8341f6648b9fd
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-04-05 02:44:33 +0200

    PR feedback

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { FormsModule }  from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { SignupRoutingModule } from './signup-routing.module';
+
 import { SignupComponent } from './signup.component';
 
 @NgModule({
